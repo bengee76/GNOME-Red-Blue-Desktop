8f1b31174ffa633c7aec9b4748e25ee36347e637
bengee76 <beniamin8r@gmail.com>
2024-09-04 11:30:52 +0200

    commit

--- configure.sh
+++ configure.sh
@@ -17,20 +17,36 @@ doNames() {
     sed -i "${2}s/.*/${count}/" "$current"
 }
 
-currentDir=$(pwd)
+doColors() {
+    palette="['#171421', '#c01c28', '#c01c28', '#a2734c', '#12488b', '#a347ba', '#2aa1b3', '#d0cfcc', '#5e5c64', '#f66151', '#33d17a', '#e9ad0c', '#2a7bde', '#c061cb', '#33c7de', '#ffffff']"
+
+    profileUUID=$(gsettings get org.gnome.Terminal.ProfilesList default | tr -d "'")
+
+    gsettings set "org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$profileUUID/" use-theme-colors false
+    gsettings set "org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$profileUUID/" bold-color-same-as-fg false
+
+    gsettings set "org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$profileUUID/" background-color "#000000"
+    gsettings set "org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$profileUUID/" foreground-color "#FFFFFF"
+    gsettings set "org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$profileUUID/" bold-color "#C01C28"
+    gsettings set "org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$profileUUID/" palette "$palette"
+}
+
+doBind() {
+    gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings \
+    "['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/', '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/']"
+
+    gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ name "Change template"
+    gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ command "bash $currentDir/templateChanger.sh"
+    gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ binding "<Ctrl><Alt>r"
+
+    gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/ name "Change wallpaper"
+    gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/ command "bash $currentDir/wallpaperChanger.sh"
+    gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/ binding "<Ctrl><Alt>t"
+}
 
 gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
 
-gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings \
-"['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/', '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/']"
-
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ name "Change template"
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ command "bash $currentDir/templateChanger.sh"
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ binding "<Ctrl><Alt>r"
-
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/ name "Change wallpaper"
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/ command "bash $currentDir/wallpaperChanger.sh"
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/ binding "<Ctrl><Alt>t"
+currentDir=$(pwd)
 
 current="variables.txt"
 
@@ -45,4 +61,10 @@ sed -i "2s/.*/-1/" "$current"
 doNames "red" 3
 doNames "blue" 4
 
+doColors
+
+doBind
+
 bash "wallpaperChanger.sh"
+
+echo "Done."
--- templateChanger.sh
+++ templateChanger.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/bash
 
+profileUUID=$(gsettings get org.gnome.Terminal.ProfilesList default | tr -d "'")
+
+redPalette="['#171421', '#c01c28', '#c01c28', '#a2734c', '#12488b', '#a347ba', '#2aa1b3', '#d0cfcc', '#5e5c64', '#f66151', '#33d17a', '#e9ad0c', '#2a7bde', '#c061cb', '#33c7de', '#ffffff']"
+bluePalette="['#171421', '#12488b', '#c01c28', '#a2734c', '#12488b', '#a347ba', '#2aa1b3', '#d0cfcc', '#5e5c64', '#f66151', '#33d17a', '#e9ad0c', '#2a7bde', '#c061cb', '#33c7de', '#ffffff']"
+
 scriptDir=$(dirname "$(readlink -f "$0")")
 file="$scriptDir/variables.txt"
 
@@ -7,8 +12,12 @@ currentTemplate=$(sed -n '1p' $file)
 
 if [ $currentTemplate = "red" ]; then
     sed -i "1s/.*/blue/" "$file"
+    gsettings set "org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$profileUUID/" bold-color "#12488b"
+    gsettings set "org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$profileUUID/" palette "$bluePalette"
 else
     sed -i "1s/.*/red/" "$file"
+    gsettings set "org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$profileUUID/" bold-color "#C01C28"
+    gsettings set "org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$profileUUID/" palette "$redPalette"
 fi
 
 sed -i "2s/.*/-1/" "$file"
